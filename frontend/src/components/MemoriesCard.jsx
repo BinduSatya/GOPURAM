6b0afca51c742c3170cd6db38e698ee55e6fcbe8
BinduSatya <bsamarnadh@gmail.com>
2025-08-04 13:04:44 +0530

    favicons and travle fallback image updated

--- frontend/src/components/MemoriesCard.jsx
+++ frontend/src/components/MemoriesCard.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router";
-import { travelImage } from "/travel-memory.png";
 import { getCleanDay } from "../lib/utils";
 
 const MemoriesCard = ({ memory }) => {
@@ -19,7 +18,7 @@ const MemoriesCard = ({ memory }) => {
               memory.image === null ||
               memory.image === "" ||
               memory.image === "image place holder text"
-                ? "/travel-memory.png"
+                ? "travel-memory.png"
                 : memory.image
             }
           />
