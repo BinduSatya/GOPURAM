515f3cee6ef43d0490e1505c42d15cc6d8e43abd
BinduSatya <bsamarnadh@gmail.com>
2025-09-10 16:13:40 +0530

    TO check backend is running

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,6 +69,10 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
