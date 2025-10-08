af22e899cd32face84582063f64f536aa076429d
BinduSatya <bsamarnadh@gmail.com>
2025-10-09 01:20:48 +0530

    Add a new route for testing API response

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("API is running...."));
+app.get("/trying", (req, res) => res.send("API is running in trying...."));
 
 app.use(async (req, res, next) => {
   try {
